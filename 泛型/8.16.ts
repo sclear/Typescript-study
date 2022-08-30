c22ed9ca08bd73de2488eda979779b202e585f0b
jiangchao <1392227049@qq.com>
2022-08-30 11:09:51 +0800

    feat: type gymnast

---
+++ 泛型/8.16.ts
@@ -0,0 +1,12 @@
+// 提取Promise<T> 类型
+type GetValue<T> = T extends Promise<infer R> ? R : never;
+
+type PromiseType = Promise<number[]>;
+
+type res = GetValue<PromiseType>;
+
+type IfAny<T, K, U> = 0 extends 1 & T ? K : U;
+
+type Op = IfAny<unknown, number, string>;
+
+// 数组类型的模式匹配
---
+++ 泛型/8.22.ts
@@ -0,0 +1,45 @@
+// 包含于  ⊆    extends
+// 交集    ∩    &
+// 并集    ∪    |
+// 空集    ∅    never
+// 全集    U    unknown
+
+interface A {
+  name: string;
+  age: number;
+}
+
+interface B {
+  name: string;
+  height: number;
+}
+
+type C = A & B;
+
+const option: C = {
+  name: "1",
+  age: 1,
+  height: 1,
+};
+
+type D = A | B;
+
+// 匹配模式
+type Pop<T extends unknown[]> = T extends [...infer Rest, infer R] ? Rest : R;
+
+type test = [1, 2, 34];
+
+type PopTest = Pop<test>;
+
+//
+interface Pa {
+  name: string;
+  sex: number;
+  weight: number;
+}
+
+type ClonePa = ["name", "sex"][number];
+
+type Tip<T extends { length: number }> = T;
+
+type NewTip = Tip<string>;
